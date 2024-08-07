7cd8a93844f74ff75049383c1d386ded2bb36bf9
jennifermel <jennifermelelissa8503@gmail.com>
2024-08-07 16:55:58 -0300

    reira itens da lista

---
+++ main.py
@@ -0,0 +1,22 @@
+#lista de compras
+compras = ['Ovos','Macarrão','Feijão','Arroz','Leite','Chocolate']
+
+#mostrar lista na tela
+for i in range(len(compras)):
+    print(f'Índice {i}: {compras[i]}.')
+
+#tratamento de exceçoes try
+try:
+    #usuario informa o item que deseja retirar
+    indice = int(input('\nÍndice do item que deseja retirar: '))
+    #retira item da lista
+    del(compras[indice])
+    print('\nItem retirado com sucesso.')
+except:
+    print('\nNão foi possível excluir.')
+finally:
+    #finally
+    for i in range(len(compras)):
+        print(f'Índice {i}: {compras[i]}.')
+
+
